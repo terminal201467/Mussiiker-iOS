65cb779493bf1255de31882f54b496948dc07ef8
MJ <terminal201467@gmail.com>
2023-01-12 08:28:46 +0800

    main:(UI)design UI Flow in Mainly in LearnerPage

---
+++ Mussiiker/InApp/ViewController/Learner/FindTeacherViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FindTeacherViewController.swift
+//  Mussiiker
+//
+//  Created by Jhen Mu on 2023/1/12.
+//
+
+import UIKit
+
+class FindTeacherViewController: UIViewController {
+    
+    //尋找老師：可以針對專長進行尋找
+    //瀏覽老師：
+    //推薦音樂老師：
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Mussiiker/InApp/ViewController/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  Mussiiker
+//
+//  Created by Jhen Mu on 2023/1/12.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    
+    //FindTeacherViewController
+    
+    
+    //FindMusicClassRoomController
+    
+    
+    //FindMusicGroupController
+       //searchBar
+       //找樂手
+       //找社團
+       //找
+    
+    
+    //RecentActivityViewController
+       //CollectionView(近期活動資訊）
+       //TableView(依照使用者有興趣的部分）
+       
+       //右上方也許會有個鍵：找不到有興趣的活動嗎?
+       //點擊之後進到搜尋頁面
+
+}
